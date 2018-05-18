9064ab03c4ae51809171507766e406bfd7e3616b
AlexeiMet <freelanceall@mail.ru>
2018-05-18 14:17:57 +0300

    build[webapck]: comment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Game from './pages/Game';
 import MainMenu from './pages/MainMenu';
+import Header from './components/Header';
 
 class App extends React.Component {
   public render() {
     return (
-      <div>
-        <ul>
-          <li><Link to='/game'>Game</Link></li>
-          <li><Link to='/main-menu'>MainMenu</Link></li>
-        </ul>
+      <React.Fragment>
+        <Header />
         <Switch>
           <Route path='/game' component={Game} />
           <Route path='/main-menu' component={MainMenu} />
         </Switch>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import App from './App';
+import AppRoutings from './AppRoutings';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<AppRoutings />, document.getElementById('root'));

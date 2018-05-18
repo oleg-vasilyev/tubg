bcc24628d02ec5ea89885ac00f36aae5de161806
Andrei Ksianzou <mulatmar@mail.ru>
2018-05-18 16:22:37 +0300

    feat: add new properties on Config

--- src/classes/Config.ts
+++ src/classes/Config.ts
@@ -4,5 +4,7 @@ export const CONFIG = {
   killsScore: 1,
   surviveScore: 0.5,
   onEnemykillScore: 2,
-  aiProcessingLimit: 100
+  aiProcessingLimit: 100,
+  simulationTimeLimit: 50000,
+  speedMultiplier: 10
 }

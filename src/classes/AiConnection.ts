53768092799db869f03a35ea21a259d7f8ef210c
Andrei Ksianzou <mulatmar@mail.ru>
2018-05-20 18:16:24 +0300

    fix: AiConnection

--- src/classes/AiConnection.ts
+++ src/classes/AiConnection.ts
@@ -1,32 +1,31 @@
-import { Tank } from './Tank';
+import { ICallbackArg, ICommandAi } from '../interfaces/interfaces';
+import { CONFIG } from './Config';
 import { IdentificatorAi } from './IdentificatorAi';
-import { ICommandAi, IRejectCallbackArg } from '../interfaces/interfaces';
-import { CONFIG } from './Config'
+import { Tank } from './Tank';
+
 /**
- * 
  * @class of AI connection
- * 
  */
 export class AiConnection {
-  
+
   public tank: Tank;
   public identificatorAi: IdentificatorAi;
   public aiProcessingStart: number;
   public aiProcessingLimit: number;
-  public aiProcessingCheckInterval: any;
-  public aiProcessingResolveCallback: ((arg: IRejectCallbackArg)=>void) | null;
-  public aiProcessingRejectCallback: ((arg: IRejectCallbackArg)=>void) | null;
+  public aiProcessingCheckInterval: NodeJS.Timer;
+  public aiProcessingResolveCallback: ((arg: ICallbackArg) => void) | null;
+  public aiProcessingRejectCallback: ((arg: ICallbackArg) => void) | null;
   public isReady: boolean;
   public commandData: ICommandAi;
   public aiWorker: Worker | null;
-  public onActivationCallback: Array<()=>{}>;
-  public onDectivationCallback: Array<()=>{}>;
+  public onActivationCallback: Array<() => void>;
+  public onDectivationCallback: Array<() => void>;
   public status: string;
   /**
    * @param {identificatorAi} identificatorAi - identification of tank's AI Script
    * @param {Tank} tank - tank
    */
-  constructor(tank: Tank, identificatorAi: IdentificatorAi) {
+  public constructor(tank: Tank, identificatorAi: IdentificatorAi) {
     this.tank = tank;
     this.aiProcessingStart = 0;
     this.aiProcessingLimit = CONFIG.aiProcessingLimit;
@@ -37,7 +36,7 @@ export class AiConnection {
     this.identificatorAi = identificatorAi;
     this.isReady = false;
     this.aiWorker = null;
-    this.status = "activate";
+    this.status = 'activate';
     this.commandData = {
       move: false,
       shoot: false,
@@ -45,21 +44,21 @@ export class AiConnection {
     };
   }
 
-  setProcessingLimit(t: number): void {
+  public setProcessingLimit(t: number): void {
     this.aiProcessingLimit = t;
   }
 
-  getTank(): Tank {
+  public getTank(): Tank {
     return this.tank;
   }
 
-  commandTank(command: ICommandAi): void {
+  public commandTank(command: ICommandAi): void {
 
     this.commandData = {
       move: command.move,
       shoot: command.shoot,
       rotate: command.rotate
-    }
+    };
 
     if (this.commandData.shoot) {
       this.tank.shoot();
@@ -71,27 +70,27 @@ export class AiConnection {
       this.commandData.move = false;
     }
 
-    if(this.commandData.rotate) {
+    if (this.commandData.rotate) {
       this.tank.rotate(this.commandData.rotate);
     }
-    
   }
 
-  xhRequest() { //Connect connect to AI file on XHR request
-    let xhr: XMLHttpRequest = new XMLHttpRequest();
+  public xhRequest(): XMLHttpRequest { // connect connect to AI file on XHR request
+    const xhr: XMLHttpRequest = new XMLHttpRequest();
+
     return xhr;
   }
 
-  sendRequest(resolve: ()=>void, reject?: ()=>void) {
-    let xhr: XMLHttpRequest = this.xhRequest();
-    let stateJSON: string = JSON.stringify(this.tank.state);
-    xhr.open("POST", `${this.identificatorAi.getPathAi}`, true);
+  public sendRequest(resolve: () => void, reject?: () => void): void {
+    const xhr: XMLHttpRequest = this.xhRequest();
+    const stateJSON: string = JSON.stringify(this.tank.state);
+    xhr.open('POST', `${this.identificatorAi.getPathAi}`, true);
     xhr.timeout = this.aiProcessingLimit;
     this.aiProcessingRejectCallback = reject;
     xhr.ontimeout = () => {
-      if(this.aiProcessingRejectCallback !== null) {
+      if (this.aiProcessingRejectCallback !== null) {
         this.aiProcessingRejectCallback({
-          message: "Simulation cannot be continued because " + this.tank.name + " #" + this.tank.id + " does not respond",
+          message: 'Simulation cannot be continued because ' + this.tank.name + ' #' + this.tank.id + ' does not respond',
           performanceIssues: true,
           tankName: this.tank.name,
           tankId: this.tank.id
@@ -99,13 +98,15 @@ export class AiConnection {
         this.aiProcessingResolveCallback = null;
         this.aiProcessingRejectCallback = null;
       }
-    }
-    xhr.setRequestHeader("Content-Type", "application/json");
+    };
+    xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify(stateJSON));
     xhr.onreadystatechange = () => {
-      if (xhr.readyState != 4) return;
-      if (xhr.status != 200) {
-        if(this.aiProcessingRejectCallback) {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      if (xhr.status !== 200) {
+        if (this.aiProcessingRejectCallback) {
           this.aiProcessingRejectCallback({
             message: "XMLHTTPRequest to '" + this.tank.name + "' is status different from 200",
             performanceIssues: false,
@@ -118,17 +119,17 @@ export class AiConnection {
           resolve();
           this.commandData = JSON.parse(xhr.responseText);
         }
-      } 
-    }
+      }
+    };
   }
 
-  activateXHR(resolve: ()=>{}, reject?: ()=>{}) {
+  public activateXHR(resolve: () => void, reject?: () => void): void {
     this.simulationStepXHR(resolve, reject);
-    this.status = "simulationStep";
+    this.status = 'simulationStep';
   }
 
-  simulationStepXHR(resolve: ()=>{}, reject?: ()=>{}): void {
-    if (this.tank.health == 0) {
+  public simulationStepXHR(resolve: () => void, reject?: () => void): void {
+    if (this.tank.health === 0) {
       return;
     } else {
       this.aiProcessingResolveCallback = resolve;
@@ -139,21 +140,19 @@ export class AiConnection {
 
 
   /**
-   * 
-   * Next code for webworker
-   * 
+   * next code for webworker
    */
-  createWorker(aiIdent: IdentificatorAi): Worker { //Connect to ai from file in Webworker
+  public createWorker(aiIdent: IdentificatorAi): Worker { // connect to ai from file in Webworker
     return new Worker(aiIdent.getPathAi());
   }
 
-  activate(resolve: ()=>{}, reject?: ()=>{}): void {
+  public activate(resolve: () => void , reject?: () => void): void {
     this.aiWorker = this.createWorker(this.identificatorAi);
     this.aiWorker.onerror = (err) => {
       console.error(err);
-      if(this.aiProcessingRejectCallback) {
+      if (this.aiProcessingRejectCallback) {
         this.aiProcessingRejectCallback({
-          message: "Web Worker of '" + this.tank.name + "' returned an error",
+          message: 'Web Worker of ' + this.tank.name + ' returned an error',
           performanceIssues: false,
           tankName: this.tank.name,
           tankId: this.tank.id
@@ -162,45 +161,49 @@ export class AiConnection {
         this.aiProcessingRejectCallback = null;
       }
     };
-    if(this.aiProcessingCheckInterval) {
+    if (this.aiProcessingCheckInterval) {
       clearInterval(this.aiProcessingCheckInterval);
       this.aiProcessingCheckInterval = null;
     }
 
-    this.aiProcessingCheckInterval = setInterval(() => {
-      if(this.aiProcessingRejectCallback !== null) {
-        let now = (new Date()).getTime();
-        let dt = now - this.aiProcessingStart;
-        if (dt > this.aiProcessingLimit) {
-          clearInterval(this.aiProcessingCheckInterval);
-          this.aiProcessingCheckInterval = null;
-          this.aiProcessingRejectCallback({
-            message: "Simulation cannot be continued because " + this.tank.name + " #" + this.tank.id + " does not respond",
-            performanceIssues: true,
-            tankName: this.tank.name,
-            tankId: this.tank.id
-          });
+    this.aiProcessingCheckInterval = setInterval(
+      () => {
+        if (this.aiProcessingRejectCallback !== null) {
+          const now = (new Date()).getTime();
+          const dt = now - this.aiProcessingStart;
+          if (dt > this.aiProcessingLimit) {
+            clearInterval(this.aiProcessingCheckInterval);
+            this.aiProcessingCheckInterval = null;
+            this.aiProcessingRejectCallback({
+              message: 'Simulation cannot be continued because ' + this.tank.name + ' #' + this.tank.id + ' does not respond',
+              performanceIssues: true,
+              tankName: this.tank.name,
+              tankId: this.tank.id
+            });
+          }
         }
-      }
-    }, Math.max(this.identificatorAi.loadingLimit, Math.round(this.aiProcessingLimit/2)));
+      },
+      Math.max(this.identificatorAi.loadingLimit, Math.round(this.aiProcessingLimit / 2))
+  );
 
     this.aiWorker.onmessage = (commandEvent) => {
       if (this.aiProcessingResolveCallback !== null) {
         this.isReady = true;
         this.commandTank(commandEvent.data);
         let callback;
-        let now = (new Date()).getTime();
-        let dt = now - this.aiProcessingStart;
-        if (dt > this.identificatorAi.loadingLimit && this.aiProcessingRejectCallback !== null) { 
+        const now = (new Date()).getTime();
+        const dt = now - this.aiProcessingStart;
+        if (dt > this.identificatorAi.loadingLimit && this.aiProcessingRejectCallback !== null) {
           callback = this.aiProcessingRejectCallback;
           this.aiProcessingResolveCallback = null;
           this.aiProcessingRejectCallback = null;
           callback({
-            message: "Simulation cannot be continued because " + this.tank.name + " #" + this.tank.id + " has performance issues",
+            message: 'Simulation cannot be continued because ' + this.tank.name + ' #' + this.tank.id + ' has performance issues',
             performanceIssues: true,
             tankName: this.tank.name,
             tankId: this.tank.id
           });
+
           return;
         }
         callback = this.aiProcessingResolveCallback;
@@ -212,21 +215,22 @@ export class AiConnection {
     this.aiProcessingResolveCallback = resolve;
     this.aiProcessingRejectCallback = reject;
     this.aiWorker.postMessage(this.tank.state);
-    this.status = "simulationStep";
+    this.status = 'simulationStep';
   }
 
-  deactivate(): void {
+  public deactivate(): void {
     if (this.aiWorker) {
       this.aiWorker.terminate();
       this.aiWorker = null;
     }
   }
 
-  simulationStep(resolve: ()=>void, reject?: ()=>void): void {
-    if (this.aiWorker && this.tank.health == 0) {
+  public simulationStep(resolve: () => void, reject?: () => void): void {
+    if (this.aiWorker && this.tank.health === 0) {
       this.aiWorker.terminate();
       this.aiWorker = null;
       resolve();
+
       return;
     }
     this.aiProcessingStart = (new Date()).getTime();
